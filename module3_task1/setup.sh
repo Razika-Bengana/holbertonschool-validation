f6a1fdb7308f0b1c95405abf8e0ed73d6b60b02d
Razika-Bengana <5003@holbertonstudents.com>
2023-01-08 20:01:26 +0100

    update setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -6,4 +6,5 @@ apt-get install -y make
 apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 apt-get install ./install_hugo.deb
-make build
+apt-get install -y make
+exit 0
